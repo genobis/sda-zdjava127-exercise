804fc254fa588b11fd944b242dd52ec33a46be43
Mariusz2022M <ziomal_swir@o2.pl>
2022-12-09 17:54:23 +0100

    Dodana klasa Grade

---
+++ src/main/java/pl/sdacademy/java/exercise/Grade.java
@@ -0,0 +1,45 @@
+package pl.sdacademy.java.exercise;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Grade {
+    private UUID courseUuid;
+    private GradeType gradeType;
+    private LocalDateTime dateTime;
+    private BigDecimal value;
+
+    public Grade(UUID courseUuid, GradeType gradeType, LocalDateTime dateTime, BigDecimal value) {
+        this.courseUuid = courseUuid;
+        this.gradeType = gradeType;
+        this.dateTime = dateTime;
+        this.value = value;
+    }
+
+    public UUID getCourseUuid() {
+        return courseUuid;
+    }
+
+    public GradeType getGradeType() {
+        return gradeType;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "courseUuid=" + courseUuid +
+                ", gradeType=" + gradeType +
+                ", dateTime=" + dateTime +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/main/java/pl/sdacademy/java/exercise/GradeType.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.java.exercise;
+
+public enum GradeType {
+    BOARD,
+    QUIZZ,
+    FINAL;
+
+}
